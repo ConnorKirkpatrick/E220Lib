4bc62ea76a9b350a2ec1e6a0970e6769daf34f74
connor <ConnorKirkpatrick3@gmail.com>
2022-02-04 17:01:18 +0000

    added missing semicolon to line 30, added explanation to software vs hardware config on line 22

--- examples/configuration/configuration.ino
+++ examples/configuration/configuration.ino
@@ -19,6 +19,7 @@ void setup() {
     //create our module
     Stream &mySerial3 = (Stream &)Serial3;
     E220 radioModule(&mySerial3, m0, m1, aux);
+    ///Use 2 above lines for a hardware serial interface, use the Line below for a software serial interface
     //E220 radioModule(receiver, m0, m1, aux);
 
     //initialise the module and check it communicates with us, else loop and keep trying
@@ -27,7 +28,7 @@ void setup() {
     }
     //setting a permanent variable for the module
     radioModule.setAddress(1234,true);
-    Serial.println(radioModule.getAddress())
+    Serial.println(radioModule.getAddress());
 
     //setting a non-permanent variable for the module
     radioModule.setPower(Power_21,false);
